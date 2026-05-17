33050efabaf40c17e1491b766134eed9d98ca0ef
rodrigorodrigues0311-rgb <rodrigo.rodrigues0311@gmail.com>
2026-05-17 16:37:06 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,38 @@
-// CSN Cimentos v1778986604
-const CACHE_NAME='csn-v1778986604';
-const URLS=['./','./index.html','./manifest.json','./sw.js','./icon-192.png','./icon-512.png','./lima-logo.png'];
-self.addEventListener('install',function(e){self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(function(c){return c.addAll(URLS).catch(function(){});}).catch(function(){}));});
-self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(ns){return Promise.all(ns.filter(function(n){return n!==CACHE_NAME;}).map(function(n){return caches.delete(n);}))}).then(function(){return self.clients.claim();}))});
-self.addEventListener('fetch',function(e){if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(function(r){if(r.ok){var c=r.clone();caches.open(CACHE_NAME).then(function(ca){ca.put(e.request,c);});}return r;}).catch(function(){return caches.match(e.request);}))});
+// CSN Cimentos Manutencao v2
+const CACHE_NAME = 'csn-manut-v2';
+const URLS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', function(e){
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(c){
+      return c.addAll(URLS).catch(function(){});
+    })
+  );
+});
+
+self.addEventListener('activate', function(e){
+  e.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(
+        keys.filter(function(k){ return k !== CACHE_NAME; })
+            .map(function(k){ return caches.delete(k); })
+      );
+    }).then(function(){ return self.clients.claim(); })
+  );
+});
+
+self.addEventListener('fetch', function(e){
+  if(e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(function(r){
+      if(r.ok){
+        var c = r.clone();
+        caches.open(CACHE_NAME).then(function(ca){ ca.put(e.request, c); });
+      }
+      return r;
+    }).catch(function(){
+      return caches.match(e.request);
+    })
+  );
+});
