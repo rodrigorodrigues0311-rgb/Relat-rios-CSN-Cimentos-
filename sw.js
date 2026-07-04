671685dbce67ec4f8b86672b50495dba1e772e09
rodrigorodrigues0311-rgb <rodrigo.rodrigues0311@gmail.com>
2026-07-04 18:00:38 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// CSN Cimentos Manutencao v2
-const CACHE_NAME = 'csn-manut-v2';
+// CSN Cimentos Manutencao v3
+const CACHE_NAME = 'csn-manut-v3';
 const URLS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function(e){
@@ -25,7 +25,9 @@ self.addEventListener('activate', function(e){
 self.addEventListener('fetch', function(e){
   if(e.request.method !== 'GET') return;
   e.respondWith(
-    fetch(e.request).then(function(r){
+    // cache:'no-store' forca ignorar o cache HTTP do navegador/GitHub Pages,
+    // garantindo que o arquivo mais recente do repositorio seja sempre buscado.
+    fetch(e.request, {cache: 'no-store'}).then(function(r){
       if(r.ok){
         var c = r.clone();
         caches.open(CACHE_NAME).then(function(ca){ ca.put(e.request, c); });
